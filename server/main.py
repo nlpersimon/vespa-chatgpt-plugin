afd3b7b1c83a5b9d1fe19ca6acca46949fec3ac0
Simon Lee <simon2020291@gmail.com>
2023-05-20 18:45:28 +0800

    feat: support local plugin

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 import os
 import uvicorn
-from fastapi import FastAPI, HTTPException, Depends, Body
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import FastAPI, HTTPException, Body
+from starlette.responses import FileResponse
 
 from models.api import (
     UpsertRequest,
@@ -10,19 +10,45 @@ from models.api import (
     QueryResponse,
 )
 from datastore.factory import get_datastore
-
-bearer_scheme = HTTPBearer()
-BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
-assert BEARER_TOKEN is not None
+from fastapi.middleware.cors import CORSMiddleware
 
 
-def validate_token(credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
-    if credentials.scheme != "Bearer" or credentials.credentials != BEARER_TOKEN:
-        raise HTTPException(status_code=401, detail="Invalid or missing token")
-    return credentials
+app = FastAPI()
 
 
-app = FastAPI(dependencies=[Depends(validate_token)])
+origins = [
+    f"http://localhost:{8001}",
+    "https://chat.openai.com",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.route("/.well-known/ai-plugin.json")
+async def get_manifest(request):
+    file_path = "./.well-known/ai-plugin.json"
+    simple_headers = {}
+    simple_headers["Access-Control-Allow-Private-Network"] = "true"
+    return FileResponse(file_path, media_type="text/json", headers=simple_headers)
+
+
+@app.route("/.well-known/logo.png")
+async def get_logo(request):
+    file_path = "./.well-known/logo.png"
+    return FileResponse(file_path, media_type="text/json")
+
+
+@app.route("/.well-known/openapi.yaml")
+async def get_openapi(request):
+    file_path = "./.well-known/openapi.yaml"
+    return FileResponse(file_path, media_type="text/json")
 
 
 @app.post(
@@ -64,4 +90,4 @@ async def startup():
 
 
 def start():
-    uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server.main:app", host="127.0.0.1", port=8001, reload=True)
